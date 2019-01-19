a1eed8164f011e734115a6b406250ebf76cd3e13
Обвинцев Слава <clava1096@gmail.com>
2019-01-19 15:09:14 +0500

    idea commit

---
+++ src/main/java/net/thumbtack/school/introduction/FirstSteps.java
@@ -0,0 +1,56 @@
+package net.thumbtack.school.introduction;
+
+public class FirstSteps{
+public int sum(int x , int y){ return 0; }
+    public int mul(int x, int y){ return 0; }
+        public int div(int x, int y){ return 0; }
+            public int mod (int x , int y){ return 0; }
+                public boolean isEqual(int x, int y){ return true; }
+                    public boolean isGreater (int x, int y){
+                        return true;
+                    }
+public boolean isInsideRect(int xLeft, int yTop, int xRight, int yButtom, int x, int y){
+    return true;
+}
+    public int sum(int[] array){
+        return 0;
+    }
+        public int mul(int[] array){
+            return 0;
+        }
+            public int min(int[] array){
+                return 0;
+            }
+                public int max (int[] array){
+                    return 0;
+                }
+public double average(int[] array){
+    return 0;
+}
+public boolean isSortedDescendant(int[] array){
+    return true;
+}
+public void cube(int[]array){ }
+public boolean find(int[]array, int value){
+    return true;
+}
+public void reserve(int[]array){
+    return ;
+}
+public boolean isPalindrome(int[]array){
+    return true;
+}
+public int sum(int[][] matrix){
+    return 0;
+}
+public int max(int[][] matrix){
+        return 0;
+    }
+    public int diagonalMax(int[][] matrix){
+            return 0;
+        }
+        public boolean isSortedDescendant(int[][] matrix){
+                return true;
+            }
+
+}
