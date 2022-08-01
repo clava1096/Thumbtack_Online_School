dd9cb7e66a52d1bd6a32284499dcee31be9883de
sl1 <clava1096@gmail.com>
2022-08-01 21:46:44 +0500

    running the course from the beginning... tasks are all done

---
+++ src/main/java/net/thumbtack/school/pictures/v1/RectPicture.java
@@ -0,0 +1,116 @@
+package net.thumbtack.school.pictures.v1;
+
+import java.util.Objects;
+
+public class RectPicture {
+    private Point topLeft, bottomRight;
+    private int format, width, height;
+    public RectPicture(Point topLeft, Point bottomRight, int format){
+        this.topLeft= topLeft;
+        this.bottomRight = bottomRight;
+        this.width = bottomRight.getX()-topLeft.getX()+1;
+        this.height = bottomRight.getY()-topLeft.getY()+1;
+        this.format = format;
+    }
+
+    public RectPicture(int xLeft, int yTop, int width, int height, int format){
+        this.width = width;
+        this.format = format;
+        this.height = height;
+        topLeft = new Point(xLeft,yTop);
+        bottomRight = new Point(width+xLeft-1, yTop+height-1);
+    }
+    public RectPicture(Point topLeft, Point bottomRight){
+        format = 1;
+        this.topLeft = topLeft;
+        this.bottomRight = bottomRight;
+        this.width = bottomRight.getX()-topLeft.getX()+1;
+        this.height = bottomRight.getY() - topLeft.getY()+1;
+    }
+    public RectPicture(int xLeft, int yTop, int width, int height){
+        this.topLeft = new Point(xLeft, yTop);
+        this.bottomRight = new Point(width+xLeft-1, yTop+height-1);
+        this.width = width;
+        this.height = height;
+        this.format = 1;
+    }
+    public Point getTopLeft(){
+        return topLeft;
+    }
+    public Point getBottomRight(){
+        return bottomRight;
+    }
+    public int getFormat(){
+        return format;
+    }
+    public void setTopLeft(Point topLeft){
+        this.topLeft = topLeft;
+    }
+    public void setBottomRight(Point bottomRight){
+        this.bottomRight = bottomRight;
+    }
+    public int getWidth(){
+        return width;
+    }
+    public int getHeight(){
+        return height;
+    }
+    public void moveTo(int x, int y){
+        topLeft.moveTo(x,y);
+        this.bottomRight = new Point(width+x-1, y+height-1);
+    }
+    public void moveTo(Point point){
+        topLeft.moveTo(point.getX(),point.getY());
+        bottomRight = new Point(width+point.getX()-1, point.getY()+height-1);
+    }
+    public void moveRel(int dx, int dy){
+        topLeft.moveRel(dx, dy);
+        bottomRight.moveRel(dx, dy);
+    }
+    public void resize(double ratio) {
+        int old_h= height ,old_w= width;
+        this.height = (int)(this.height*ratio);
+        if(height == 0) {height = 1;}
+        this.width = (int)(this.width*ratio);
+        if(width == 0) {width =1;}
+        bottomRight = new Point(bottomRight.getX()-old_w+width, bottomRight.getY()-old_h+height);
+    }
+    public boolean isInside(int x, int y){
+        if (bottomRight.getX()>=x & bottomRight.getY() >=y & x >= topLeft.getX() & y >= topLeft.getY()) { return true;}
+        else return false;
+    }
+    public boolean isInside(Point point) {
+        if (bottomRight.getX()>=point.getX() & bottomRight.getY() >= point.getY() & point.getX() >= topLeft.getX() & point.getY() >= topLeft.getY()) { return true;}
+        else return false;
+    }
+
+    public boolean isIntersects(RectPicture rectPicture){
+        if (bottomRight.getY() <  rectPicture.topLeft.getY() || rectPicture.bottomRight.getY() < topLeft.getY()) return false;
+        if (bottomRight.getX() < rectPicture.topLeft.getX() || rectPicture.bottomRight.getX() < topLeft.getX()) return  false;
+        return true;
+    }
+    public boolean isInside(RectPicture rectPicture){
+        return (rectPicture.bottomRight.getX() <= bottomRight.getX() && topLeft.getX() <=rectPicture.topLeft.getX() &&
+                rectPicture.bottomRight.getY() <= bottomRight.getY() && topLeft.getY() <= rectPicture.topLeft.getY());
+    }
+
+    public boolean isFullyVisibleOnDesktop(Desktop desktop) {
+        //if (desktop.getWidth() >= getWidth()-topLeft.getX() && desktop.getHeight() >= getHeight()-topLeft.getY()) return true;
+        //return false;
+        if (topLeft.getY()>=0 & topLeft.getX() >=0 & bottomRight.getX()<640 & bottomRight.getY()<480) return true;
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RectPicture that = (RectPicture) o;
+        return format == that.format && width == that.width && height == that.height && Objects.equals(topLeft, that.topLeft) && Objects.equals(bottomRight, that.bottomRight);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(topLeft, bottomRight, format, width, height);
+    }
+}
---
+++ src/main/java/net/thumbtack/school/pictures/v1/RoundPicture.java
@@ -0,0 +1,133 @@
+package net.thumbtack.school.pictures.v1;
+
+import java.util.Objects;
+
+public class RoundPicture {
+    private Point center;
+    private int format, radius;
+    public RoundPicture(Point center, int radius, int format) {
+        this.center = center;
+        this.radius = radius;
+        this.format = format;
+    }
+
+    public RoundPicture(int xCenter, int yCenter, int radius, int format) {
+        center = new Point(xCenter,yCenter);
+        this.radius = radius;
+        this.format = format;
+    }
+
+    public RoundPicture(Point center, int radius) {
+        format = 1;
+        this.center = center;
+        this.radius = radius;
+    }
+
+    public RoundPicture(int xCenter, int yCenter, int radius) {
+        center = new Point(xCenter,yCenter);
+        format = 1;
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public int getFormat() {
+        return format;
+    }
+
+    public void moveTo(int x, int y) {
+        this.center.setX(x);
+        this.center.setY(y);
+    }
+
+    public void moveTo(Point point) {
+        this.center.setX(point.getX());
+        this.center.setY(point.getY());
+    }
+
+    public void setCenter(int x, int y) {
+        this.center.setX(x);
+        this.center.setY(y);
+    }
+
+    public void setRadius(int radius) {
+        this.radius=radius;
+    }
+
+    public void setFormat(int format) {
+        this.format = format;
+    }
+
+    public void moveRel(int dx, int dy){
+        this.center.setX(center.getX()+dx);
+        this.center.setY(center.getY()+dy);
+    }
+
+    public void resize(double ratio){
+        this.radius = (int)(this.radius*ratio);
+        if (radius == 0) radius=1;
+    }
+
+    /*cout << "Введите координаты центра окружности:" << endl;
+    int Xc, Yc;
+    cin >> Xc >> Yc;
+    cout << "Введите радиус:" << endl;
+    int r;
+    cin >> r;
+    cout << "Введите координаты точки:" << endl;
+    int x, y;
+    cin >> x >> y;
+    if (((x - Xc) * (x - Xc) + (y - Yc) * (y - Yc)) < r * r) {
+        cout << "Точка принадлежит окружности." << endl;
+    } else if (((x - Xc) * (x - Xc) + (y - Yc) * (y - Yc)) == r * r) {
+        cout << "Точка лежит на окружности." << endl;
+    } else {
+        cout << "Точка не принадлежит окружности." << endl;
+    }*/
+
+
+
+    public boolean isInside(int x, int y){
+        int Xc = center.getX(), Yc = center.getY();
+        if(((x - Xc) * (x - Xc) + (y - Yc) * (y - Yc)) <= radius * radius){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isInside(Point point){
+        int Xc = center.getX(), Yc = center.getY();
+        if(((point.getX() - Xc) * (point.getX() - Xc) + (point.getY() - Yc) * (point.getY() - Yc))<=radius*radius ){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFullyVisibleOnDesktop(Desktop desktop){
+        int xLeft = center.getX()-radius; // >=0
+        int xRight = center.getX()+radius; // <=640
+        int yUp = center.getY()+radius; // <=480
+        int yDown = center.getY()-radius; // >=0
+        if (xLeft >=0 && xRight < desktop.getWidth() && yUp <desktop.getHeight() && yDown>=0) return true;
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoundPicture that = (RoundPicture) o;
+        return format == that.format && radius == that.radius && Objects.equals(center, that.center);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(center, format, radius);
+    }
+}
